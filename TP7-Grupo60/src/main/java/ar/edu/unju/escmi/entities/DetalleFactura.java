f340a8dfa32559d4882e941719657e2f89bd65d4
none <none@DESKTOP-FKH17L5>
2024-11-01 20:41:23 -0300

    Implementacion de la clase DetalleFactura

---
+++ TP7-Grupo60/src/main/java/ar/edu/unju/escmi/entities/DetalleFactura.java
@@ -0,0 +1,61 @@
+package ar.edu.unju.escmi.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+@Entity
+@Table(name = "Detalle_Facturas")
+public class DetalleFactura {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "detalle_fact_id")
+    private Long id;
+
+    @JoinColumn(name = "detalle_fact_producto")
+    private Producto producto;
+
+    @Column(name = "detalle_fact_cantidad")
+    private int cantidad;
+
+    @Column(name = "detalle_fact_subtotal")
+    private double subtotal;
+
+    public DetalleFactura() {}
+
+    public DetalleFactura(Producto producto, int cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+        calcularSubtotal();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+        calcularSubtotal();  
+    }
+    
+    private void calcularSubtotal() {
+        if (this.producto != null && this.producto.getPrecioUnitario() > 0) {
+            this.subtotal = this.cantidad * this.producto.getPrecioUnitario();
+        } else {
+            this.subtotal = 0.0;
+        }
+    }
+}
